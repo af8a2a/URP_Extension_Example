34bba76211cd74496c261dc299d9c0fe03fd51fe
af8a2a <1125209282@qq.com>
2024-12-09 23:53:05 +0800

    checkpoint

--- Assets/Features/VolumetricLight/VolumetricLight.shader
+++ Assets/Features/VolumetricLight/VolumetricLight.shader
@@ -1,48 +1,16 @@
 ﻿Shader "Volumetric Light"
 {
     HLSLINCLUDE
-    #pragma exclude_renderers gles
+    #define MAIN_LIGHT_CALCULATE_SHADOWS  //定义阴影采样
+    #define _MAIN_LIGHT_SHADOWS_CASCADE //启用级联阴影
 
-    #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
-    #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/DeclareDepthTexture.hlsl"
     #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
-    #include "Packages/com.unity.render-pipelines.core/Runtime/Utilities/Blit.hlsl"
-
-    float4 _BlitTexture_TexelSize;
-
-    float4 _SourceTexLowMip_TexelSize;
-
-    TEXTURE2D(_SourceTexture);
-    float _Intensity;
-
-    half3 DecodeHDR(half4 color)
-    {
-        #if UNITY_COLORSPACE_GAMMA
-            color.xyz *= color.xyz; // γ to linear
-        #endif
-
-        #if _USE_RGBM
-            return DecodeRGBM(color);
-        #else
-        return color.xyz;
-        #endif
-    }
-
-    half4 EncodeHDR(half3 color)
-    {
-        #if _USE_RGBM
-            half4 outColor = EncodeRGBM(color);
-        #else
-        half4 outColor = half4(color, 1.0);
-        #endif
-
-        #if UNITY_COLORSPACE_GAMMA
-            return half4(sqrt(outColor.xyz), outColor.w); // linear to γ
-        #else
-        return outColor;
-        #endif
-    }
-
+    #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/CommonMaterial.hlsl"
+    #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Shadows.hlsl"  //阴影计算库
+    #include "Packages/com.unity.render-pipelines.universal/Shaders/PostProcessing/Common.hlsl"
+    #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+    #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/DeclareDepthTexture.hlsl"
+    #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/DeclareOpaqueTexture.hlsl"
 
 
     float3 GetWorldPosition(float3 positionHCS)
@@ -58,17 +26,41 @@
         return ComputeWorldSpacePosition(UV, depth, UNITY_MATRIX_I_VP);
     }
 
+    float GetLightAttenuation(float3 position)
+    {
+        float4 shadowPos = TransformWorldToShadowCoord(position); //把采样点的世界坐标转到阴影空间
+        float intensity = MainLightRealtimeShadow(shadowPos); //进行shadow map采样
+        return intensity; //返回阴影值
+    }
+
+    #define MAX_RAY_LENGTH 20
+    float _StepTime;
+    float _Intensity;
+
     half4 Frag(Varyings input) : SV_Target
     {
         UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(input);
-        float texelSize = _BlitTexture_TexelSize.y;
         float2 uv = UnityStereoTransformScreenSpaceTex(input.texcoord);
+        float3 worldPos = GetWorldPosition(input.positionCS); //像素的世界坐标
+        float3 startPos = _WorldSpaceCameraPos; //摄像机上的世界坐标
+        float3 dir = normalize(worldPos - startPos); //视线方向
 
-        
-        float3 worldPos = GetWorldPosition(input.positionCS);
+        float rayLength = length(worldPos - startPos); //视线长度
+        rayLength = min(rayLength, MAX_RAY_LENGTH); //限制最大步进长度，MAX_RAY_LENGTH这里设置为20
+        float3 final = startPos + dir * rayLength;
+        half3 intensity = 0; //累计光强
+        float2 step = 1.0 / _StepTime; //定义单次插值大小，_StepTime为步进次数
+        for (float i = 0; i < 1; i += step) //光线步进
+        {
+            float3 currentPosition = lerp(startPos, final, i); //当前世界坐标
+            float atten = GetLightAttenuation(currentPosition) * _Intensity; //阴影采样，_Intensity为强度因子
+            float3 light = atten;
+            intensity += light;
+        }
+        intensity /= _StepTime;
 
-
-        return half4(worldPos,1);
+        half3 color=SampleSceneColor(uv);
+        return half4(intensity, 1); //查看结果
     }
     ENDHLSL
 
@@ -92,6 +84,17 @@
             #pragma fragment Frag
             ENDHLSL
         }
+        Pass
+        {
+            Name "Blur"
+            
+            HLSLPROGRAM
+            #include "KawaseBlur.hlsl"
+            #pragma vertex vertex
+            #pragma fragment fragment
+            ENDHLSL
+        }
+
 
     }
 }
--- Assets/Features/VolumetricLight/VolumetricLightFeature.cs
+++ Assets/Features/VolumetricLight/VolumetricLightFeature.cs
@@ -1,29 +1,34 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.Serialization;
 
+[Serializable]
+public class VolumetricLightSettings
+{
+    public float Intensity;
+    public float _StepTime;
+}
+
 public class VolumetricLightFeature : ScriptableRendererFeature
 {
     public Material material;
+    public VolumetricLightSettings settings;
 
     class CustomRenderPass : ScriptableRenderPass
     {
         private Material _material;
+        private RTHandle sourceRT;
+        private VolumetricLightSettings _settings;
 
-        public CustomRenderPass(Material material)
+        public CustomRenderPass(Material material, VolumetricLightSettings settings)
         {
             _material = material;
+            _settings = settings;
         }
 
-        // This method is called before executing the render pass.
-        // It can be used to configure render targets and their clear state. Also to create temporary render target textures.
-        // When empty this render pass will render to the active camera render target.
-        // You should never call CommandBuffer.SetRenderTarget. Instead call <c>ConfigureTarget</c> and <c>ConfigureClear</c>.
-        // The render pipeline will ensure target setup and clearing happens in a performant manner.
-        public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
-        {
-        }
 
         // Here you can implement the rendering logic.
         // Use <c>ScriptableRenderContext</c> to issue drawing commands or execute command buffers
@@ -31,10 +36,30 @@ public class VolumetricLightFeature : ScriptableRendererFeature
         // You don't have to call ScriptableRenderContext.submit, the render pipeline will call it at specific points in the pipeline.
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
         {
-            var cmd = CommandBufferPool.Get();
-            
-            
-            Blit(cmd,ref renderingData, _material);
+            if (_material == null)
+            {
+                return;
+            }
+            var camera = renderingData.cameraData.camera;
+
+            if (camera.cameraType == CameraType.Preview)
+            {
+                return;
+            }
+
+
+            var cmd = CommandBufferPool.Get("VolumetricLight");
+            var desc = new RenderTextureDescriptor(renderingData.cameraData.cameraTargetDescriptor.width,
+                renderingData.cameraData.cameraTargetDescriptor.height, RenderTextureFormat.RGB111110Float);
+
+            _material.SetFloat("_StepTime", _settings._StepTime);
+            _material.SetFloat("_Intensity", _settings.Intensity);
+
+            RenderingUtils.ReAllocateIfNeeded(ref sourceRT, desc, name: "VolumetricLightRT");
+
+
+            Blit(cmd,renderingData.cameraData.renderer.cameraColorTargetHandle, sourceRT, _material, 0);
+
             context.ExecuteCommandBuffer(cmd);
             CommandBufferPool.Release(cmd);
         }
@@ -45,21 +70,30 @@ public class VolumetricLightFeature : ScriptableRendererFeature
         }
     }
 
+    public RenderPassEvent renderPassEvent = RenderPassEvent.AfterRenderingPostProcessing;
+
     CustomRenderPass m_ScriptablePass;
+
 
     /// <inheritdoc/>
     public override void Create()
     {
-        m_ScriptablePass = new CustomRenderPass(material);
+        m_ScriptablePass = new CustomRenderPass(material, settings);
 
         // Configures where the render pass should be injected.
-        m_ScriptablePass.renderPassEvent = RenderPassEvent.AfterRenderingOpaques;
+        m_ScriptablePass.renderPassEvent = renderPassEvent;
     }
 
     // Here you can inject one or multiple render passes in the renderer.
     // This method is called when setting up the renderer once per-camera.
     public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
     {
+        if (material == null)
+        {
+            SetActive(false);
+            return;
+        }
+
         renderer.EnqueuePass(m_ScriptablePass);
     }
 }
