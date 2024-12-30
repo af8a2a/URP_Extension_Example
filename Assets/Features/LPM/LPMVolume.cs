a9f784ced6483741da25383710fde0b9f4133e17
af8a2a <1125209282@qq.com>
2024-12-30 16:34:44 +0800

    fix: adjust LPM value

--- Assets/Features/LPM/LPMVolume.cs
+++ Assets/Features/LPM/LPMVolume.cs
@@ -15,10 +15,10 @@ namespace Features.LPM
     public class LPMVolume : VolumeComponent, IPostProcessComponent
     {
         public ClampedFloatParameter Intensity = new ClampedFloatParameter(1f, 0f, 1f);
-        public FloatParameter HdrMax = new FloatParameter(8.0f);
+        public FloatParameter HdrMax = new FloatParameter(256.0f);
         public ClampedFloatParameter SoftGap = new ClampedFloatParameter(0f, 0f, 1f);
         public ClampedFloatParameter Exposure = new ClampedFloatParameter(0.0f, -4.0f, 1.0f);
-        public ClampedFloatParameter LPMExposure = new ClampedFloatParameter(1.0f, 3.0f, 11.0f);
+        public ClampedFloatParameter LPMExposure = new ClampedFloatParameter(8.0f, 3.0f, 11.0f);
         public ClampedFloatParameter Contrast = new ClampedFloatParameter(0.3f, 0.0f, 1.0f);
         public ClampedFloatParameter ShoulderContrast = new ClampedFloatParameter(1.0f, 1.0f, 1.2f);
         public NoInterpVector3Parameter Saturation = new NoInterpVector3Parameter(Vector3.zero);
