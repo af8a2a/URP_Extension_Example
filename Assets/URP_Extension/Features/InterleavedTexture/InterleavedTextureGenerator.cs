4bbf1a96a1510cbdbfbdb62ee127763eb319fa9e
af8a2a <1125209282@qq.com>
2025-03-26 00:07:52 +0800

    feat: InterleavedTextureGenerator

--- Assets/URP_Extension/Features/InterleavedTexture/InterleavedTextureGenerator.cs
+++ Assets/URP_Extension/Features/InterleavedTexture/InterleavedTextureGenerator.cs
@@ -1,11 +1,36 @@
-﻿using UnityEngine;
-using UnityEngine.Experimental.Rendering;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Rendering;
 
 namespace Features.InterleavedTexture
 {
     public class InterleavedTextureGenerator
     {
-        private ComputeShader _deinterleaveCS;
+        private ComputeShader InterleavedTextureCS;
+
+        private static readonly Lazy<InterleavedTextureGenerator> _instance = new Lazy<InterleavedTextureGenerator>();
+        public static InterleavedTextureGenerator Instance => _instance.Value;
+
+        private static int _interleavedInputTextureID = Shader.PropertyToID("Input");
+        private static int _interleavedResultTextureID = Shader.PropertyToID("Result");
+
+        private int kernelID;
+
+        public InterleavedTextureGenerator()
+        {
+            InterleavedTextureCS = Resources.Load<ComputeShader>("InterleavedTexture");
+            kernelID = InterleavedTextureCS.FindKernel("PrepareInterleavedTexture");
+        }
+
+        
+        public RTHandle CreateTexture2DArray(CommandBuffer cmd, RTHandle rtInput,ref RTHandle rtOutput)
+        {
+            cmd.SetComputeTextureParam(InterleavedTextureCS, kernelID, _interleavedInputTextureID, rtInput);
+            cmd.SetComputeTextureParam(InterleavedTextureCS, kernelID, _interleavedResultTextureID, rtOutput);
+
+            cmd.DispatchCompute(InterleavedTextureCS, kernelID, 8, 8, 1);
+            
+            return rtOutput;
+        }
     }
 }
