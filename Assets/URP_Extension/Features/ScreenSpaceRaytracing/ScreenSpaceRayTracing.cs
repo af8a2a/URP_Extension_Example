56accf46a50eb29c870c2919fce1595949e9db47
af8a2a <1125209282@qq.com>
2025-03-30 19:34:00 +0800

    refactor: Screen Space Raytracing
    ForwardGbufferPass

--- Assets/URP_Extension/Features/ScreenSpaceRaytracing/ScreenSpaceRayTracing.cs
+++ Assets/URP_Extension/Features/ScreenSpaceRaytracing/ScreenSpaceRayTracing.cs
@@ -1,7 +1,20 @@
-﻿namespace URP_Extension.Features.ScreenSpaceRaytracing
+﻿using UnityEngine.Rendering.Universal;
+
+namespace URP_Extension.Features.ScreenSpaceRaytracing
 {
-    public class ScreenSpacePathTracing
+    public class ScreenSpaceRayTracing: ScriptableRendererFeature
     {
-        
+        ForwardGBufferPass m_GBufferPass;
+        private readonly string[] m_GBufferPassNames = new string[] { "UniversalGBuffer" };
+
+        public override void Create()
+        {
+            m_GBufferPass = new ForwardGBufferPass(m_GBufferPassNames);
+        }
+
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            renderer.EnqueuePass(m_GBufferPass);
+        }
     }
 }
