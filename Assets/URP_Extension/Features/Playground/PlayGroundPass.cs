e0682850aa921f599a7ea6c72d4015eb4c46efb5
af8a2a <1125209282@qq.com>
2025-04-04 23:40:25 +0800

    feat:DeinterleavedTexture Sample with ComputeShader

--- Assets/URP_Extension/Features/Playground/PlayGroundPass.cs
+++ Assets/URP_Extension/Features/Playground/PlayGroundPass.cs
@@ -11,6 +11,8 @@ namespace URP_Extension.Features.Playground
         {
             internal TextureHandle cameraTexture;
             internal TextureHandle targetTexture;
+            internal TextureHandle tempTexture;
+
             internal int width;
             internal int height;
         }
@@ -18,7 +20,10 @@ namespace URP_Extension.Features.Playground
         static void ExecutePass(PassData data, ComputeGraphContext cgContext)
         {
             var cmd = cgContext.cmd;
-            InterleavedTextureGenerator.Instance.RenderInterleavedTexture(cmd, data.cameraTexture, data.targetTexture,data.width/8,data.height/8);
+            InterleavedTextureGenerator.Instance.RenderInterleavedTexture(cmd, data.cameraTexture, data.targetTexture,
+                data.width / 8, data.height / 8);
+            InterleavedTextureGenerator.Instance.SampleInterleavedTexture(cmd, data.targetTexture, data.tempTexture,
+                data.width / 8, data.height / 8);
         }
 
         public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
@@ -32,17 +37,29 @@ namespace URP_Extension.Features.Playground
             desc.width /= 2;
             desc.height /= 2;
             desc.slices = 4;
-            desc.name = "Playground";
-            var output = renderGraph.CreateTexture(desc);
+            desc.name = "Playground_0";
+            var output0 = renderGraph.CreateTexture(desc);
+            desc.width  *= 2;
+            desc.height *= 2;
+
+            desc.dimension = TextureDimension.Tex2D;
+            desc.slices = 1;
+
+            desc.name = "Playground_1";
+
+            var output1 = renderGraph.CreateTexture(desc);
+
             using (var builder = renderGraph.AddComputePass<PassData>("Playground", out var data))
             {
                 builder.AllowPassCulling(false);
                 data.cameraTexture = cameraColor;
-                data.targetTexture = output;
+                data.targetTexture = output0;
+                data.tempTexture = output1;
                 data.width = desc.width;
                 data.height = desc.height;
                 builder.UseTexture(data.cameraTexture, AccessFlags.ReadWrite);
                 builder.UseTexture(data.targetTexture, AccessFlags.ReadWrite);
+                builder.UseTexture(data.tempTexture, AccessFlags.ReadWrite);
 
                 builder.SetRenderFunc((PassData data, ComputeGraphContext cgContext) => ExecutePass(data, cgContext));
             }
